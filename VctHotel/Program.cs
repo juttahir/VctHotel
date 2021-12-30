d943457812d6c76bf751a4183609cb14b08f5cc8
Juttahir <juttahir@hotmail.com>
2021-12-30 17:44:33 -0300

    Primeiro commit

---
+++ VctHotel/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VctHotel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quartosDisponiveis = 10;
+            Rooms[] rooms = new Rooms[quartosDisponiveis];
+
+            Console.Write("How many rooms will be rented? ");
+            int quantidade = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= quantidade; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Your name: ");
+                string name = Console.ReadLine();
+                Console.Write("Your email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int r = int.Parse(Console.ReadLine());
+                rooms[r] = new Rooms(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms: ");
+            for (int j = 0; j < quartosDisponiveis; j++)
+            {
+                if (rooms[j] != null)
+                {
+                    Console.WriteLine(j + ": " + rooms[j]);
+                }
+            }
+        }
+    }
+}
---
+++ VctHotel/Rooms.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace VctHotel
+{
+    public class Rooms
+    {
+        public string Name { get; set; }
+        public string Email { get; private set; }
+
+        public Rooms(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
